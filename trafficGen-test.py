408d3aa463ef9649a140567f62bc4a8a9c8779e5
alexfrt <aferreira91@gmail.com>
2014-03-25 21:00:27 -0300

    inclusão de parametros de inicialização

--- trafficGen-test.py
+++ trafficGen-test.py
@@ -6,10 +6,11 @@ from math import sqrt
 
 import os
 import time
+import argparse
 
 networksCounter = 0
 
-def buildNetwork(mininet, hostsCount, ipScope):
+def buildNetwork(mininet, hostsCount, ipScope, loss, bandwidth, delay):
     global networksCounter
     
     switch = mininet.addSwitch('s%s' % networksCounter)
@@ -17,27 +18,27 @@ def buildNetwork(mininet, hostsCount, ipScope):
 
     for i in range(0, hostsCount):
         host = mininet.addHost('h%s%s' % (networksCounter, i), ip = "%s.%s" % (ipScope, i))
-        mininet.addLink(host, switch)
+        mininet.addLink(host, switch) #bw = bandwidth, delay = "%sms" % delay, loss=loss
         hosts.append(host)
 
     networksCounter += 1
 
     return (switch, hosts)
 
-def setupNetworks(mininet, networkSetup):
+def setupNetworks(mininet, networkSetup, loss, bandwidth, delay):
     switches = []
     hosts = []
 
     #Build all sub networks
     for setup in networkSetup:
-        nodes = buildNetwork(mininet, networkSetup[setup], setup)
+        nodes = buildNetwork(mininet, networkSetup[setup], setup, loss, bandwidth, delay)
         switches.append(nodes[0])
         hosts.extend(nodes[1])
 
     #Connect all switchs in line
     for i, sA in enumerate(switches):
         for sB in switches[i + 1:]:
-            mininet.addLink(sA, sB)
+            mininet.addLink(sA, sB) #bw = bandwidth * 20, delay = delay / 3, loss= loss / 2
 
     return (switches, hosts)
 
@@ -109,6 +110,16 @@ def showStatistics(statistics):
 
 if __name__ == '__main__':
     setLogLevel('info')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', help='server port', default=9999)
+    parser.add_argument('-b', help='server transmission bitrate in kbps', default=2048)
+    parser.add_argument('-n', help='how many subnetworks to build', default=3)
+    parser.add_argument('-c', help='how many hosts per subnetwork', default=5)
+    parser.add_argument('--loss', help='links loss percentage', default=0)
+    parser.add_argument('--bandwidth', help='links bandwidth in mbps', default=0)
+    parser.add_argument('--delay', help='links delay in ms', default=0)
+    args = parser.parse_args()
     
     net = Mininet()
 
@@ -116,24 +127,26 @@ if __name__ == '__main__':
     net.addController('c0')
 
     info('*** Setting up networks ***\n')
+    networkSetup = {}
+    for i in range(200, 200 + int(args.n)):
+        networkSetup['192.168.%s' % i] = int(args.c)
 
-    networkSetup = {'192.168.201' : 10, '192.168.202' : 10}
     info('     Config: %s\n' % networkSetup)
         
-    nodes = setupNetworks(net, networkSetup)
+    nodes = setupNetworks(net, networkSetup, args.loss, args.bandwidth, args.delay)
 
     info('*** Starting network ***\n')
     net.start()
 
     info('*** Starting traffic server ***\n')
     server = nodes[1][0]
-    startTrafficServer(server, 9999, 3 * 1024) #3 mbps
+    startTrafficServer(server, int(args.p), int(args.b))
 
     time.sleep(2)
 
     info('*** Starting traffic clients ***\n')
     clients = nodes[1][1:]
-    startTrafficClients(clients, server.IP(), 9999)
+    startTrafficClients(clients, server.IP(), int(args.p))
 
     CLI(net)
 
